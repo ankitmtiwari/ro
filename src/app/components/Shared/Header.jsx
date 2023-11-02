d1771fe6d43f548d1d2a777add8592f208a50ef7
ankitmtiwari <tankit291389@gmail.com>
2023-11-02 21:40:22 +0530

    tmp

--- src/app/components/Shared/Header.jsx
+++ src/app/components/Shared/Header.jsx
@@ -101,7 +101,7 @@ function Header() {
                 <div className="flex align-middle items-center gap-8 ">
                   <Link href={"/"}>
                     <div className="hover:text-[#00ABFF] text-lg font-medium cursor-pointer">
-                      Home
+                      HHome
                     </div>
                   </Link>
                   <Link href={"/#home_products"}>
