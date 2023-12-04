3deeb82091c9075a3e966938a1473b2181903997
ankitmtiwari <tankit291389@gmail.com>
2023-12-04 22:10:35 +0530

    minimal changes

--- src/app/components/Shared/Fixed_Floating.jsx
+++ src/app/components/Shared/Fixed_Floating.jsx
@@ -9,7 +9,7 @@ const Fixed_Floating = () => {
 
   return (
     <>
-      <div className="hidden mr-0 sm:block sticky bottom-0 z-20  w-fit p-2 ">
+      <div className="hidden mr-0 sm:block sticky bottom-0 z-20 w-fit p-2 bg-transparent h-fit ">
         <div className="p-1 m-1 ">
           {/* <div className="bg-green-400 p-2 m-2 rounded-lg text-white font-semibold cursor-pointer">
             Whatsapp
